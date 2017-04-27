9edbf8d71a815edf8b64d1b4677265abcf600a42
fr0zensn0w <liquidsn0w@lawn-143-215-58-138.lawn.gatech.edu>
2017-04-27 17:23:25 -0400

    Removing Files
    Removing Files

---
+++ FinalProject/index.js
@@ -0,0 +1,15 @@
+
+const remote = require('electron').remote
+const main = remote.require('./main.js')
+const {shell} = require('electron')
+const fs = require('fs')
+
+
+function openPhotoGallery() {
+    // alert("open the gallery")
+    // TODO: put code in here to open the gallery, AKA open up to where the images are stored
+    fullPath = "/Users/liquidsn0w/Desktop/"
+    fullPath = __dirname + "/videos/source"
+    // shell.beep() //makes a beeping sound
+    shell.showItemInFolder(fullPath)
+}
